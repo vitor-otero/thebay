44d163391eb7332f3ae8725742a7427045a2c093
Vitor Otero <vitor@v0t.org>
2022-10-28 23:04:12 +0100

    Initial Commit

---
+++ TheBay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheBay
+//
+//  Created by Vitor Otero on 28/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheBay/TheBayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheBayApp.swift
+//  TheBay
+//
+//  Created by Vitor Otero on 28/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheBayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
